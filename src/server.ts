e2df77a9f8983e35bbcea6b8d50d78fd3c5ec07c
Alex <alx5409@gmail.com>
2025-10-29 09:34:23 +0100

    Add DB connection call in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import app from './app.js'
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
