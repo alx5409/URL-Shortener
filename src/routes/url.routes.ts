e930eadc006817aa18565577faf255eca1195c8e
Alex <alx5409@gmail.com>
2025-11-02 13:35:41 +0100

    Fix import in url.routes.ts

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import type { Request, Response } from 'express'
-import { authenticateToken } from '../middleware/auth.middleware.js';
+import { authenticateToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
