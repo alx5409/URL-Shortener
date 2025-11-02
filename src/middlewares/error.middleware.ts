afc5d2d50d9fc8cbeab5954dc96f14d058212c81
Alex <alx5409@gmail.com>
2025-11-02 13:26:18 +0100

    Add unused paremeter comment in error.middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,9 +2,9 @@ import type { Request, Response, NextFunction } from "express";
 
 export function errorHandler(
     err: any,
-    _req: Request,
+    _req: Request,          // Unused parameter
     res: Response,
-    _next: NextFunction
+    _next: NextFunction     // Unused parameter
  ) {
     console.error(err);
     res.status(err.status || 500).json({
