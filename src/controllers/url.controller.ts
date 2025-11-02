4aa145932b2cb9748659fc21879c8018ede9a1c2
Alex <alx5409@gmail.com>
2025-11-02 14:50:39 +0100

    Add doc to url.controller

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,9 +1,11 @@
 import type { Request, Response } from 'express'
 
+// Controller to handle URL shortening
 export const shortenUrl = (_req: Request, res: Response) => {
   res.json({ message: 'URL shortened!' });
 };
 
+// Controller to get all URLs for a user
 export const getUserUrls = (_req: Request, res: Response) => {
   res.json({ urls: [] });
 };
