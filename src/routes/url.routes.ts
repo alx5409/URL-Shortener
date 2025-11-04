10e7a25f93d38e77306d34baadce1cd9e60083f9
Alex <alx5409@gmail.com>
2025-11-04 18:20:33 +0100

    feat: implement JWT-based user authentication for URL shortener and redirect endpoints

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { shortenUrl, getUserUrls } from '../controllers/url.controller.js';
+import { shortenUrl, getUserUrls, redirectToOriginal } from '../controllers/url.controller.js';
 // import { authenticateToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 // Protect routes with authenticateToken middleware
 router.post('/shorten', /*authenticateToken,*/ shortenUrl);
 router.get('/my-urls', /*authenticateToken,*/ getUserUrls);
+router.get('/:slug', /*authenticateToken,*/redirectToOriginal);
 
 export default router;
